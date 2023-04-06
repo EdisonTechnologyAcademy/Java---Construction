71c5351167e3fdd711ab87ec504e30ef6b3265b3
kitudu <fc.ktdung@aptech.vn>
2023-04-06 11:49:05 +0700

    update

---
+++ examples_solution/SpringBootSecurity/HttpAuthenticationDemo/src/test/java/com/edison/httpauthenticationdemo/HttpAuthenticationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edison.httpauthenticationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpAuthenticationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
