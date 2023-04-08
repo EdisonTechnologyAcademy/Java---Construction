6b80503d87ec0db7d486c9d004c5641b2cfa8139
kitudu <fc.ktdung@aptech.vn>
2023-04-08 22:57:32 +0700

    update

--- practicals_solution/Prac01Less25/src/main/java/vn/edu/vnu/java/prac01less25/RoleName.java
+++
@@ -1,6 +0,0 @@
-package vn.edu.vnu.java.prac01less25;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- practicals_solution/Prac02Less23/src/main/java/vn/edu/vnu/java/prac02less23/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.vnu.java.prac02less23.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vn.edu.vnu.java.prac02less23.model.Product;
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
