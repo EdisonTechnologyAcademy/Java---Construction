13f2aa5bc25ea683c5a71a29a4da6e09148a67d1
kitudu <fc.ktdung@aptech.vn>
2023-04-12 16:57:59 +0700

    update

---
+++ practicals_solution/Prac04lLess24/src/main/java/com/edison/Prac04lLess24/Prac04lLess24Application.java
@@ -0,0 +1,13 @@
+package com.edison.Prac04lLess24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac04lLess24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prac04lLess24Application.class, args);
+	}
+
+}
---
+++ practicals_solution/Prac04lLess24/src/test/java/com/edison/Prac04lLess24/Prac04lLess24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edison.Prac04lLess24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac04lLess24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
