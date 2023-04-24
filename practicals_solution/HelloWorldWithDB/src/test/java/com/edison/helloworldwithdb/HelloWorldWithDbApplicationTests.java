55cbdc3dcdac7b7d93129b1ea1c4e7ee59d10238
kitudu <kitudu@tlu.edu.vn>
2023-04-24 13:26:04 +0700

    Update less26

---
+++ practicals_solution/HelloWorldWithDB/src/test/java/com/edison/helloworldwithdb/HelloWorldWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edison.helloworldwithdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
